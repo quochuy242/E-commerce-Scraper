3e0904098f52ea086b92ca079aff1398cb68d724
quochuy242 <qhuy9777@gmail.com>
2024-08-31 17:03:57 +0700

    Fix error about scraping image url when a product don't have the colorway picker

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import asyncio
 import json
 from collections import namedtuple
-from typing import List
+from typing import List, Dict
 
 import httpx
 from pydantic import BaseModel
 from selectolax.parser import HTMLParser
 from tqdm import tqdm
+from urllib.parse import urljoin
 
 HEADER = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0"
@@ -14,15 +15,14 @@ HEADER = {
 
 ALL_PRODUCT_URL = "https://www.nike.com/vn/w/mens-shoes-nik1zy7ok"
 
-ImageURL = namedtuple("ImageURL", ["color", "link"])
 
 
 class Product(BaseModel):
     title: str
     subtitle: str
     price: int
-    image_url: List[ImageURL]
-    available_size: List[str]
+    url: str
+    image_url: Dict[str, str]
 
 
 async def get_html(url: str, client: httpx.AsyncClient) -> str:
@@ -47,32 +47,36 @@ def convert_price(price: str) -> int:
     return int(price.replace("₫", "").replace(",", ""))
 
 
-def get_image_url(html: HTMLParser) -> List[ImageURL]:
-    images = html.css_first("div#hero-image").css("img")
-    return [
-        ImageURL(image.attributes["alt"].split(" - ")[-1], image.attributes["src"])
-        for image in images
-    ]
+def get_image_url(html: HTMLParser) -> Dict[str, str]:
+    images = html.css_first("div#colorway-picker-container").css("a img")
+    if images: 
+        return {
+            image.attributes["alt"]: image.attributes["src"]
+            for image in images
+        }
+    else: 
+        images: str = html.css_first("div#hero-image").css_first("img").attributes['src']
+        images_resize = images.replace('_1728_', '_144_')
+        return {'Default': images_resize}
+        
 
 
 async def extract_product(url: str, client: httpx.AsyncClient) -> Product:
     html = await get_html(url, client)
+    # print(url)
     return Product(
         title=html.css_first("h1#pdp_product_title").text(),
         subtitle=html.css_first("h1#pdp_product_subtitle").text(),
         price=convert_price(html.css_first("div#price-container").text()),
+        url=url,
         image_url=get_image_url(html),
-        available_size=[
-            div.css_first("label").text()
-            for div in html.css("div[data-testid=pdp-grid-selector-item]")
-        ],
     )
 
 
 def save_to_json(products: List[Product], filename: str):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(
-            [product.dict() for product in products], f, ensure_ascii=False, indent=4
+            [product.model_dump() for product in products], f, ensure_ascii=False, indent=4
         )
 
 
@@ -80,7 +84,7 @@ async def main():
     async with httpx.AsyncClient() as client:
         html = await get_html(ALL_PRODUCT_URL, client)
         urls = get_product_url(html)
-        tasks = [extract_product(url, client) for url in tqdm(urls, desc="Extracting")]
+        tasks = [extract_product(url, client) for url in urls]
         products = await asyncio.gather(*tasks)
 
     # Save data to JSON file
